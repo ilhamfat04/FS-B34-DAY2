458d0d057f35057bdb8f6076d229907be54ee890
= <=>
2022-05-13 10:48:34 +0700

    update

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,13 @@
 // import necessary utility from rrd
-
+import { Outlet, Navigate } from 'react-router-dom'
 
 // create component here
+const PrivateRoute = () => {
+    const isSignIn = true;
+
+    return (
+        isSignIn ? <Outlet /> : <Navigate to="/signin" />
+    )
+}
+
+export default PrivateRoute
